94bd30112d2609f4aaa723ce217a4a283dc0baea
AdnaneAD1 <sidiamadouadnane4@gmail.com>
2026-01-22 02:13:58 +0100

    musique and error

--- src/services/music.js
+++ src/services/music.js
@@ -7,7 +7,7 @@ const nodes = [{
     name: 'MainNode',
     url: `${process.env.LAVALINK_HOST}:${process.env.LAVALINK_PORT}`,
     auth: process.env.LAVALINK_PASSWORD,
-    secure: process.env.LAVALINK_SECURE === 'true'
+    secure: false
 }];
 
 let kazagumo;
